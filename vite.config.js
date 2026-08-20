d5563a4c885bae62955e8eb7986f403863bb950d
Jean Jacques Goldman <goldman003926@gmail.com>
2026-08-20 15:12:33 +0300

    Fix Vercel base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+/* global process */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/goldman-raf-portfolio/",
+  base: process.env.GITHUB_ACTIONS ? "/goldman-raf-portfolio/" : "/",
 });
