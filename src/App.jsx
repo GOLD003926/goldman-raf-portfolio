d079a2dd4366f8ecf47f1b81d7b65417589280e0
Jean Jacques Goldman <goldman003926@gmail.com>
2026-08-20 14:44:43 +0300

    Init site: setup Vite + React avec page d'accueil

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        margin: 0,
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h1>Hi, I'm Goldman</h1>
+    </div>
+  );
+}
+
+export default App;
